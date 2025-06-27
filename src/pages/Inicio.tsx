7986ff301fbef6dc173bc2adbb6655a0e508428f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 11:29:15 -0600

    Agregando la otra seccion

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero";
 import Servicios from "@/components/Servicios";
+import FondoHero from "@/components/ui/FondoHero";
 
 export default function Inicio() {
     return (
         <>
+            <FondoHero />
             <Hero />
             <Servicios />
+            <section className="min-h-screen"></section>
         </>
     );
 }
