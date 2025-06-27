54204ca633ffdf46297fa9d9e2cf386d3cfff014
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 14:19:14 -0600

    Mejorar la aniamcion

--- src/components/Hero/ImagenAnimada.tsx
+++ src/components/Hero/ImagenAnimada.tsx
@@ -8,7 +8,7 @@ export default function ImagenAnimada() {
             className="absolute -z-10 w-full h-full bottom-0 left-1/2 xl:left-3/4 -translate-x-1/2 flex xl:items-center items-end xl:justify-center xl:flex-1 "
             initial={{ opacity: 0, scale: 0.95 }}
             whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.6, delay: 1.5 }}
+            transition={{ duration: 0.6, delay: 1 }}
         >
             <img src={HERO} alt="hero" className="w-full h-1/2 xl:h-11/12 object-contain -z-40" />
         </motion.div>
