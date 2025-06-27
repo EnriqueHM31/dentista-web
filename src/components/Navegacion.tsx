fa4ce9a13d5b16ab5f4200e3036d362d7744beed
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-26 22:22:34 -0600

    Mejorar a desktop

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -10,7 +10,7 @@ export default function Navegacion() {
     const { isOpen, toggleMenu } = useOpen()
 
     return (
-        <nav className='flex justify-between items-center py-4 px-6 fixed top-0 left-0 right-0 z-50 backdrop-blur-xs'>
+        <nav className='flex justify-between items-center py-4 px-6 fixed top-0 left-0 right-0 z-50 backdrop-blur-xs xl:max-w-10/12 w-full xl:mx-auto'>
             <div className='flex items-center justify-center gap-2'>
                 <AiOutlineDingtalk className='text-4xl text-primary' />
                 <h1 className='text-lg font-semibold'>Dentista LE</h1>
@@ -19,7 +19,7 @@ export default function Navegacion() {
             <MenuLateral isOpen={isOpen} toggleMenu={toggleMenu} />
 
             <button
-                className={` p-1 size-10 rounded-2xl flex items-center justify-center z-100 overflow-hidden relative ${isOpen ? 'bg-white text-primary' : 'bg-primary text-white'}`}
+                className={` p-1 size-10 rounded-2xl flex items-center justify-center z-100 overflow-hidden xl:hidden relative ${isOpen ? 'bg-white text-primary' : 'bg-primary text-white'}`}
                 onClick={toggleMenu}
                 type="button"
                 aria-expanded={isOpen}
