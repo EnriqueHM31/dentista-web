b333a10d1ff2d00a2b84c25d1951c9067702ffcf
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 14:19:24 -0600

    Colocar los componentes creados y refactorzar

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,10 @@
 import Hero from "@/components/Hero";
 import Servicios from "@/components/Servicios";
 import FondoHero from "@/components/ui/FondoHero";
+import SectionTelefono from "@/components/SectionTelefono";
+import SectionVentajas from "@/components/SectionVentajas";
+
+
 
 export default function Inicio() {
     return (
@@ -8,7 +12,9 @@ export default function Inicio() {
             <FondoHero />
             <Hero />
             <Servicios />
-            <section className="min-h-screen"></section>
+            <SectionTelefono />
+            <SectionVentajas />
+
         </>
     );
 }
