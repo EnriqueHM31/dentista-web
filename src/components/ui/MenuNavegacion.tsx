79004c83f2911a613844e1ec1b48ac976c7cebf5
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 11:28:52 -0600

    Mejorar el menu en movil y desktop

--- src/components/ui/MenuNavegacion.tsx
+++ src/components/ui/MenuNavegacion.tsx
@@ -1,13 +1,17 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { Link, useLocation } from "react-router-dom";
 import { VARIANTES_MENU, LINKS_NAVEGACION } from "@/assets/ts/constantes";
+import type { MenuNavegacionProps } from "@/types";
 
-export default function MenuLateral({ isOpen, toggleMenu }: { isOpen: boolean, toggleMenu: () => void; }) {
+export default function MenuLateral({ isOpen, toggleMenu, clases }: MenuNavegacionProps) {
     const location = useLocation();
+
+    const { textColor, buttonClasses, buttonMovilClasses, menubackground } = clases;
+
 
     return (
         <>
-            {/* 📱 Menú MÓVIL animado con Framer Motion */}
+            {/* 📱 Menú MÓVIL */}
             <AnimatePresence>
                 {isOpen && (
                     <motion.ul
@@ -16,22 +20,24 @@ export default function MenuLateral({ isOpen, toggleMenu }: { isOpen: boolean, t
                         initial="hidden"
                         animate="visible"
                         exit="exit"
-                        className="fixed top-0 right-0 z-50 h-screen w-3/4 max-w-full bg-primary text-white flex flex-col gap-6 justify-center px-6 xl:hidden"
+                        className={`fixed top-0 right-0 z-50 h-screen w-3/4 max-w-full ${menubackground} flex flex-col gap-6 justify-center px-6 xl:hidden`}
                     >
                         {LINKS_NAVEGACION.map((link) => (
                             <li key={link.name}>
                                 <Link
                                     to={link.path}
+                                    onClick={toggleMenu}
                                     className={`block py-2 text-lg transition-colors duration-300 hover:text-accent ${location.pathname === link.path ? "font-bold" : ""
                                         }`}
-                                    onClick={toggleMenu}
                                 >
                                     {link.name}
                                 </Link>
                             </li>
                         ))}
                         <li>
-                            <button className="mt-4 bg-white text-primary font-semibold px-4 py-2 rounded-xl w-full transition-transform hover:scale-105">
+                            <button
+                                className={`mt-4 font-semibold px-4 py-2 rounded-xl w-full transition-all duration-300 ease-in-out hover:scale-105 cursor-pointer ${buttonMovilClasses}`}
+                            >
                                 Realizar una cita
                             </button>
                         </li>
@@ -39,35 +45,38 @@ export default function MenuLateral({ isOpen, toggleMenu }: { isOpen: boolean, t
                 )}
             </AnimatePresence>
 
-            {/* 💻 Menú ESCRITORIO sin animaciones (siempre visible) */}
-            <ul className="hidden xl:flex gap-8 items-center">
+            {/* 💻 Menú ESCRITORIO */}
+            <ul
+                className={`hidden xl:flex gap-8 items-center transition-colors duration-300 ${textColor}`}
+            >
                 {LINKS_NAVEGACION.map((link) => (
                     <li key={link.name} className="relative">
                         <Link
                             to={link.path}
-                            className={`
-                                relative text-black hover:text-accent transition-colors duration-300 py-1
-                                ${location.pathname === link.path ? "font-bold after:w-full" : "after:w-0"}
-                                after:content-['']
-                                after:block
-                                after:absolute
-                                after:h-0.5
-                                after:bg-primary
-                                after:bottom-0
-                                after:left-0
-                                after:rounded-full
-                                after:transition-all
-                                after:duration-300
-                                after:ease-in-out
-                                hover:after:w-full
-                            `}
+                            className={`relative hover:text-accent transition-colors duration-300 py-1 ${location.pathname === link.path ? "font-bold after:w-full" : "after:w-0"
+                                }
+                after:content-['']
+                after:block
+                after:absolute
+                after:h-0.5
+                after:bg-primary
+                after:bottom-0
+                after:left-0
+                after:rounded-full
+                after:transition-all
+                after:duration-300
+                after:ease-in-out
+                hover:after:w-full
+              `}
                         >
                             {link.name}
                         </Link>
                     </li>
                 ))}
                 <li>
-                    <button className="bg-primary cursor-pointer text-white font-semibold px-4 py-1 text-md rounded-xl transition-transform hover:scale-105">
+                    <button
+                        className={`font-semibold px-4 py-1 text-md rounded-xl transition-all duration-300 ease-in-out hover:scale-105 ${buttonClasses} cursor-pointer`}
+                    >
                         Realizar una cita
                     </button>
                 </li>
