cad1ae7745f34c80a8b2bbfea5e344457b3ec67f
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 11:50:33 -0600

    Mejorar el tamaño de l nav

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -32,50 +32,55 @@ export default function Navegacion() {
         : "bg-white text-black";
     const clasesLogoIcono = scrolled ? "text-primary" : "text-white";
     const clasesLogoText = scrolled ? "text-black" : "text-white";
+    const BackgrounAfter = scrolled ? "after:bg-primary" : "after:bg-white";
 
     return (
-        <nav className='flex justify-between items-center py-4 px-6 xl:px-10 fixed top-0 left-0 right-0 z-50 backdrop-blur-xs xl:max-w-10/12 w-full xl:mx-auto'>
-            <div className='flex items-center justify-center gap-2'>
-                <AiOutlineDingtalk className={`text-4xl  ${clasesLogoIcono}`} />
-                <h1 className={`text-lg font-semibold ${clasesLogoText}`}>Dentista LE</h1>
+        <nav className={`flex justify-center items-center py-4 px-6 xl:px-10 fixed top-0 left-0 right-0 z-50 backdrop-blur-xs xl:max-w-full w-full xl:mx-auto ${scrolled ? 'bg-white' : 'bg-primary'}`}>
+
+            <div className="flex items-center justify-between gap-2 max-w-10/12 w-full">
+
+                <div className='flex items-center justify-center gap-2'>
+                    <AiOutlineDingtalk className={`text-4xl  ${clasesLogoIcono}`} />
+                    <h1 className={`text-lg font-semibold ${clasesLogoText}`}>Dentista LE</h1>
+                </div>
+
+                <MenuNavegacion isOpen={isOpen} toggleMenu={toggleMenu} clases={{ textColor, buttonClasses, buttonMovilClasses, menubackground, BackgrounAfter }} />
+
+                <button
+                    className={` p-1 size-10 rounded-2xl flex items-center justify-center z-100 overflow-hidden xl:hidden relative ${isOpen ? 'bg-white text-primary' : 'bg-primary text-white'}`}
+                    onClick={toggleMenu}
+                    type="button"
+                    aria-expanded={isOpen}
+                >
+                    <AnimatePresence mode="wait" initial={false}>
+                        {
+                            isOpen ? (
+                                <motion.div
+                                    key="close"
+                                    initial={{ x: 20, opacity: 0 }}
+                                    animate={{ x: 0, opacity: 1 }}
+                                    exit={{ x: -20, opacity: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="absolute"
+                                >
+                                    <CgClose className={`text-2xl  font-bold`} />
+                                </motion.div>
+                            ) : (
+                                <motion.div
+                                    key="menu"
+                                    initial={{ x: -20, opacity: 0 }}
+                                    animate={{ x: 0, opacity: 1 }}
+                                    exit={{ x: 20, opacity: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                    className="absolute"
+                                >
+                                    <RiMenu3Fill className={`text-2xl  font-bold`} />
+                                </motion.div>
+                            )}
+                    </AnimatePresence>
+                </button>
+
             </div>
-
-            <MenuNavegacion isOpen={isOpen} toggleMenu={toggleMenu} clases={{ textColor, buttonClasses, buttonMovilClasses, menubackground }} />
-
-            <button
-                className={` p-1 size-10 rounded-2xl flex items-center justify-center z-100 overflow-hidden xl:hidden relative ${isOpen ? 'bg-white text-primary' : 'bg-primary text-white'}`}
-                onClick={toggleMenu}
-                type="button"
-                aria-expanded={isOpen}
-            >
-                <AnimatePresence mode="wait" initial={false}>
-                    {
-                        isOpen ? (
-                            <motion.div
-                                key="close"
-                                initial={{ x: 20, opacity: 0 }}
-                                animate={{ x: 0, opacity: 1 }}
-                                exit={{ x: -20, opacity: 0 }}
-                                transition={{ duration: 0.3 }}
-                                className="absolute"
-                            >
-                                <CgClose className={`text-2xl  font-bold`} />
-                            </motion.div>
-                        ) : (
-                            <motion.div
-                                key="menu"
-                                initial={{ x: -20, opacity: 0 }}
-                                animate={{ x: 0, opacity: 1 }}
-                                exit={{ x: 20, opacity: 0 }}
-                                transition={{ duration: 0.3 }}
-                                className="absolute"
-                            >
-                                <RiMenu3Fill className={`text-2xl  font-bold`} />
-                            </motion.div>
-                        )}
-                </AnimatePresence>
-            </button>
-
 
 
         </nav >
