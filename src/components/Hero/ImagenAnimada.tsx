c62f426bbc4179a4791a6f9659931fe92b66659a
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 11:28:29 -0600

    Modificar la vista de la imagen del hero

--- src/components/Hero/ImagenAnimada.tsx
+++ src/components/Hero/ImagenAnimada.tsx
@@ -1,25 +1,16 @@
 import { motion } from "framer-motion";
-import ELIPSE from '@/assets/hero/elipse.webp';
-import ELIPSE2 from '@/assets/hero/elipse2.webp';
-import DENTISTA from '@/assets/hero/dentista.webp';
-import ICONOS from '@/assets/hero/iconos.webp';
+import HERO from '@/assets/hero/hero.png';
 
 
 export default function ImagenAnimada() {
     return (
         <motion.div
-            className="w-full h-40"
+            className="absolute -z-10 w-full h-full bottom-0 left-1/2 xl:left-3/4 -translate-x-1/2 flex xl:items-center items-end xl:justify-center xl:flex-1 "
             initial={{ opacity: 0, scale: 0.95 }}
             whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.6 }}
+            transition={{ duration: 0.6, delay: 1.5 }}
         >
-            <div className="relative w-4/6 mx-auto h-full">
-                <img src={ELIPSE} alt="elipse" className="w-full h-full absolute object-cover z-0" />
-                <img src={ELIPSE2} alt="elipse2" className="w-full h-full absolute object-cover z-10" />
-                <img src={DENTISTA} alt="dentista" className="w-full h-full absolute object-contain z-20" />
-                <img src={ICONOS} alt="iconos" className="w-full h-full absolute object-contain z-30" />
-                <div className='absolute bg-gradient-to-t from-white from-5% to-accent to-20% w-full h-full z-40 flex flex-col justify-center items-center'></div>
-            </div>
+            <img src={HERO} alt="hero" className="w-full h-1/2 xl:h-11/12 object-contain -z-40" />
         </motion.div>
     );
 }
