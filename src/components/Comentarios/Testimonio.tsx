29341001e129b2dbbb7480eca3f8816ab642c83e
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 15:57:47 -0600

    Componente card para crear un testimonio

---
+++ src/components/Comentarios/Testimonio.tsx
@@ -0,0 +1,39 @@
+import StartsTestimonials from "./StartsTestimonials.";
+
+interface TestimonioProps {
+    client_name: string;
+    rating: number;
+    comment: string;
+    index: number;
+}
+
+
+export default function Testimonio({ client_name, rating, comment, index }: TestimonioProps) {
+    function getRandomPortraitUrl(index: number) {
+        const gender = Math.random() < 0.5 ? "men" : "women";
+        return `https://randomuser.me/api/portraits/${gender}/${index}.jpg`;
+    }
+
+
+
+
+    return (
+
+        <li className="flex flex-col gap-3  px-8 bg-primary text-white rounded-2xl min-h-[30dvh] justify-between "
+        >
+            <div className="flex-1 flex items-center gap-4 ">
+                <img src={getRandomPortraitUrl(index)} alt="cliente" className="size-10 rounded-full object-cover" />
+
+                <div>
+                    <h2 className="text-3xl font-bold">{client_name}</h2>
+                    <StartsTestimonials numero={rating} />
+                </div>
+            </div>
+            <div className="flex-1 text-start">
+                <p>{comment}</p>
+            </div>
+        </li >
+    )
+}
+
+
