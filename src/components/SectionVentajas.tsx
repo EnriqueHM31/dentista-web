f33cda1482212f5535d682a1bc9548efd1f1ec25
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 14:33:19 -0600

    mejorar la seccion de ventajas

--- src/components/SectionVentajas.tsx
+++ src/components/SectionVentajas.tsx
@@ -6,7 +6,7 @@ import TituloSeccion from "./ui/TituloSeccion";
 
 export default function SectionVentajas() {
     return (
-        <motion.section className="max-w-11/12 md:max-w-10/12 w-full mx-auto py-10 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-10  gap-12 flex items-center min-h-[70dvh] bg-secondary flex-col xl:flex-row"
+        <motion.section className="max-w-11/12 md:max-w-10/12 w-full mx-auto py-10 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-10  gap-12 flex items-center min-h-[70dvh] bg-secondary flex-col xl:flex-row overflow-x-hidden"
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }} transition={{
                 duration: 0.3,
@@ -14,22 +14,11 @@ export default function SectionVentajas() {
             }}
             viewport={{ once: true, amount: 0.3 }}
         >
-            <motion.picture className="flex-1 flex items-center justify-center"
-                initial={{ opacity: 0, x: -100 }}
-                whileInView={{ opacity: 1, x: 0 }}
-                transition={{
-                    duration: 0.3,
-                    ease: "easeOut",
-                }}
+            <picture className="flex-1 flex items-center justify-center"
             >
                 <img src={SERCIVIO2} alt="Imagen servicio 2" className="w-full max-w-full xl:max-w-3/5 h-full object-cover" />
-            </motion.picture>
-            <motion.div className="flex-1 flex flex-col gap-4 px-6 xl:px-0"
-                initial={{ opacity: 0, x: 100 }}
-                whileInView={{ opacity: 1, x: 0 }} transition={{
-                    duration: 0.3,
-                    ease: "easeOut",
-                }}
+            </picture>
+            <div className="flex-1 flex flex-col gap-4 px-6 xl:px-0"
             >
                 <TituloSeccion titulo="¿Por qué elegir Smile para todos tus tratamientos dentales?" clases=" font-bold text-white" />
                 <p className="text-white/70">Utilizamos solo los materiales de la mejor calidad en el mercado para ofrecer los mejores productos a nuestros pacientes.</p>
@@ -45,7 +34,7 @@ export default function SectionVentajas() {
                 </ul>
 
                 <button className="w-fit rounded-lg px-3 py-2 mt-7 bg-white text-primary hover:bg-white/80 transition duration-300 ease-in-out cursor-pointer ">Realizar una cita</button>
-            </motion.div>
+            </div>
         </motion.section>
     )
 }
