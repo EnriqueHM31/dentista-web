a4e44974cb609789f8a890e0e8d598c7dbc6f5f3
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 19:13:25 -0600

    Componer el responsive de la seccion de telefono

--- src/components/SectionTelefono.tsx
+++ src/components/SectionTelefono.tsx
@@ -4,7 +4,7 @@ import TituloSeccion from "./ui/TituloSeccion";
 
 export default function SectionTelefono() {
     return (
-        <motion.section className="min-h-screen flex xl:flex-row flex-col items-center justify-center max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-10  gap-12"
+        <motion.section className="min-h-screen flex xl:flex-row flex-col items-center justify-center max-w-11/12 md:max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-10  gap-12"
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }} transition={{
                 duration: 0.3,
@@ -19,7 +19,7 @@ export default function SectionTelefono() {
 
                 <motion.p>Utilizamos solo los materiales de la mejor calidad en el mercado para ofrecer los mejores productos a nuestros pacientes, así que no te preocupes por nada y reserva tu cita.</motion.p>
 
-                <motion.form action="#" className="flex items-center gap-5 w-full max-w-3/4"
+                <motion.form action="#" className="flex items-center gap-5 w-full max-w-full md:max-w-3/4"
                     initial={{ opacity: 0, y: 30 }}
                     whileInView={{ opacity: 1, y: 0 }} transition={{
                         duration: 0.3,
@@ -41,7 +41,7 @@ export default function SectionTelefono() {
                     ease: "easeOut",
                 }}
             >
-                <div className="size-90 relative">
+                <div className="xl:size-90 md:size-75 size-45 w-full max-w-11/12 relative">
                     <div className="absolute -z-10 w-full h-full inset-0 flex xl:items-center items-end xl:justify-center xl:flex-1 bg-gradient-to-tr to-primary from-indigo-950 from-60% translate-x-7"></div>
                     <img src={IMAGENSERVICIO1} alt="Imagen servicio 1" className="w-full h-full object-cover translate-y-5 " />
                 </div>
--- src/components/ui/TituloSeccion.tsx
+++ src/components/ui/TituloSeccion.tsx
@@ -6,6 +6,6 @@ interface TituloSeccionProps {
 
 export default function TituloSeccion({ titulo, clases }: TituloSeccionProps) {
     return (
-        <h2 className={`text-4xl w-full font-bold text-accent ${clases}`}>{titulo}</h2>
+        <h2 className={`xl:text-4xl lg:text-3xl md:text-2xl text-2xl w-full font-bold text-accent ${clases}`}>{titulo}</h2>
     )
 }
