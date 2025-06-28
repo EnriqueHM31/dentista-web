a51fecc39cec6cfeb41259cb98ef05e062a559b6
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-27 18:57:36 -0600

    Mejorar el uso del carusel

--- src/components/SectionEspecialistas.tsx
+++ src/components/SectionEspecialistas.tsx
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion";
 import TituloSeccion from "./ui/TituloSeccion";
-import { Slider } from "./Especialistas/Slider";
+import Carousel from "./ui/carousel";
+import { DATASLIDERESPECIALISTAS } from "@/assets/ts/constantes";
 
 export default function SectionEspecialistas() {
     return (
-        <motion.section className="min-h-screen flex flex-col items-center justify-center max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-10 gap-4"
+        <motion.section className="min-h-screen flex flex-col items-center justify-center max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-0 gap-4"
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }} transition={{
                 duration: 0.3,
@@ -16,9 +17,17 @@ export default function SectionEspecialistas() {
             <p className="max-w-3/4 w-full text-center">Utilizamos únicamente los materiales de la más alta calidad disponibles en el mercado para brindar los mejores tratamientos a nuestros pacientes.</p>
 
 
-            <div>
-                <Slider />
-            </div>
+            <motion.div className="flex flex-col items-center justify-center py-15 overflow-x-hidden w-full"
+                initial={{ opacity: 0, y: 30 }}
+                whileInView={{ opacity: 1, y: 0 }} transition={{
+                    duration: 0.3,
+                    ease: "easeOut",
+                }}
+                viewport={{ once: true, amount: 0.3 }}
+            >
+
+                <Carousel slides={DATASLIDERESPECIALISTAS} />
+            </motion.div>
         </motion.section>
     )
 }
