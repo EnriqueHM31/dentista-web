28257f946b0c9111ec69f5c2002b9ca68fd03e6c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 14:31:45 -0600

    Mejorar la seccion de especialistas

--- src/components/SectionEspecialistas.tsx
+++ src/components/SectionEspecialistas.tsx
@@ -5,7 +5,7 @@ import { DATASLIDERESPECIALISTAS } from "@/assets/ts/constantes";
 
 export default function SectionEspecialistas() {
     return (
-        <motion.section className="min-h-screen flex flex-col items-center justify-center max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-0 gap-4"
+        <motion.section className="min-h-screen flex flex-col items-center justify-center max-w-11/12 xl:max-w-10/12 w-full mx-auto py-5 xl:py-10 px-1 md:px-10 lg:px-20 xl:px-0 gap-4"
             initial={{ opacity: 0, y: 30 }}
             whileInView={{ opacity: 1, y: 0 }} transition={{
                 duration: 0.3,
@@ -14,18 +14,11 @@ export default function SectionEspecialistas() {
             viewport={{ once: true, amount: 0.3 }}
         >
             <TituloSeccion titulo="Conoce a nuestros especialistas" clases="text-center" />
-            <p className="max-w-3/4 w-full text-center">Utilizamos únicamente los materiales de la más alta calidad disponibles en el mercado para brindar los mejores tratamientos a nuestros pacientes.</p>
+            <p className="max-w-full xl:max-w-3/4 w-full text-center">Utilizamos únicamente los materiales de la más alta calidad disponibles en el mercado para brindar los mejores tratamientos a nuestros pacientes.</p>
 
 
-            <motion.div className="flex flex-col items-center justify-center py-15 overflow-x-hidden w-full"
-                initial={{ opacity: 0, y: 30 }}
-                whileInView={{ opacity: 1, y: 0 }} transition={{
-                    duration: 0.3,
-                    ease: "easeOut",
-                }}
-                viewport={{ once: true, amount: 0.3 }}
+            <motion.div className="flex flex-col items-center justify-center relative overflow-x-hidden w-full pb-20     md:max-w-10/12"
             >
-
                 <Carousel slides={DATASLIDERESPECIALISTAS} />
             </motion.div>
         </motion.section>
