b9353481fd0006b03421ede897dc0a6c37445dfd
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 20:13:42 -0600

    Custom hook del uso de carrusel

---
+++ src/hooks/useCarrusel.ts
@@ -0,0 +1,29 @@
+import type { SlideData } from "@/types";
+import { useState } from "react";
+
+export function useCarrusel({ slides }: { slides: SlideData[] }) {
+    const [current, setCurrent] = useState(0);
+
+    const handlePreviousClick = () => {
+        const previous = current - 1;
+        setCurrent(previous < 0 ? slides.length - 1 : previous);
+    };
+
+    const handleNextClick = () => {
+        const next = current + 1;
+        setCurrent(next === slides.length ? 0 : next);
+    };
+
+    const handleSlideClick = (index: number) => {
+        if (current !== index) {
+            setCurrent(index);
+        }
+    };
+
+    return {
+        current,
+        handlePreviousClick,
+        handleNextClick,
+        handleSlideClick,
+    };
+}
