7bf331d3b8cfee0b567ceb30853160d5bec722a3
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 22:46:28 -0600

    Modificar y crear el componente para responsive

--- src/components/general/Modal.tsx
+++ src/components/general/Modal.tsx
@@ -1,13 +1,15 @@
 // components/ui/Modal.tsx
 import { AnimatePresence, motion } from "framer-motion";
+import { CgClose } from "react-icons/cg";
 
 interface ModalProps {
     isOpen: boolean;
     onClose: () => void;
     children: React.ReactNode;
+    clases?: string;
 }
 
-export default function Modal({ isOpen, onClose, children }: ModalProps) {
+export default function Modal({ isOpen, onClose, children, clases }: ModalProps) {
     return (
         <AnimatePresence>
             {isOpen && (
@@ -18,7 +20,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
                     exit={{ opacity: 0 }}
                 >
                     <motion.div
-                        className="bg-white rounded-xl shadow-xl p-6 max-w-md w-full relative"
+                        className={`${clases} bg-white rounded-xl shadow-xl  w-full relative max-h-[90vh] overflow-auto`}
                         initial={{ scale: 0.8, opacity: 0, y: 50 }}
                         animate={{ scale: 1, opacity: 1, y: 0 }}
                         exit={{ scale: 0.8, opacity: 0, y: 50 }}
@@ -27,9 +29,9 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
                         {/* Cerrar */}
                         <button
                             onClick={onClose}
-                            className="absolute top-2 right-2 text-black hover:text-red-500 text-xl font-bold"
+                            className="absolute top-4 right-4 text-black hover:text-red-500 text-2xl font-bold cursor-pointer bg-white rounded-full p-2"
                         >
-                            ×
+                            <CgClose />
                         </button>
 
                         {children}
