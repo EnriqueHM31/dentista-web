bcca0d44e84c16fc0886faa2853bd3bb4eda5b7d
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-06-28 20:10:57 -0600

    Colocar el custom hooks scroll

--- src/components/Navegacion.tsx
+++ src/components/Navegacion.tsx
@@ -4,41 +4,12 @@ import { CgClose } from "react-icons/cg";
 import { motion, AnimatePresence } from 'framer-motion'
 import MenuNavegacion from './ui/MenuNavegacion';
 import { useOpen } from "@/hooks/useOpen";
-import { useEffect, useState } from "react";
+import { useScroll } from "@/hooks/useScroll";
 
 export default function Navegacion() {
 
     const { isOpen, toggleMenu } = useOpen()
-    const [scrolled, setScrolled] = useState(false);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrolled(window.scrollY > window.innerHeight - 100);
-        };
-
-
-        window.addEventListener("scroll", handleScroll);
-        window.addEventListener("resize", handleScroll);
-        window.addEventListener("orientationchange", handleScroll);
-        window.addEventListener("load", handleScroll);
-        return () => window.removeEventListener("scroll", handleScroll);
-
-    }, []);
-
-    const textColor = scrolled ? "text-black" : "text-white";
-    const buttonClasses = scrolled
-        ? "bg-primary text-white"
-        : "bg-white text-primary";
-    const buttonMovilClasses = scrolled
-        ? "bg-white text-primary"
-        : "bg-primary text-white";
-    const menubackground = scrolled
-        ? "bg-primary text-white"
-        : "bg-white text-black";
-    const clasesLogoIcono = scrolled ? "text-primary" : "text-white";
-    const clasesLogoText = scrolled ? "text-black" : "text-white";
-    const BackgrounAfter = scrolled ? "after:bg-primary" : "after:bg-white";
-    const hoverColor = !scrolled ? "hover:text-white/70" : "hover:text-black/70";
+    const { scrolled, clasesLogoIcono, clasesLogoText, menubackground, textColor, buttonClasses, buttonMovilClasses, BackgrounAfter, hoverColor } = useScroll()
 
     return (
         <nav className={`flex justify-center items-center py-4 px-6 xl:px-10 fixed top-0 left-0 right-0 z-50 backdrop-blur-xs xl:max-w-full w-full xl:mx-auto ${scrolled ? 'bg-white' : 'bg-primary'}`}>
