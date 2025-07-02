b4714264dd30439789feac2d8507eebe992ad26c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-02 11:24:24 -0600

    Agregar el tipado al archivo

--- Frontend/src/types.d.ts
+++ Frontend/src/types.d.ts
@@ -43,3 +43,72 @@ interface SlideProps {
     current: number;
     handleSlideClick: (index: number) => void;
 }
+
+interface ModalCrearProps {
+    toggle: () => void;
+    handleCrearPregunta: (e: React.FormEvent<HTMLFormElement>) => void;
+    pregunta: string;
+    respuesta: string;
+    setPregunta: React.Dispatch<React.SetStateAction<string>>;
+    setRespuesta: React.Dispatch<React.SetStateAction<string>>;
+}
+
+interface Pregunta {
+    id: number;
+    pregunta: string;
+    respuesta: string;
+}
+
+interface ModalEditarProps {
+    preguntaSeleccionada: Pregunta | null;
+    toggle: () => void;
+    handleEditarPregunta: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    handleEditarRespuesta: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+    handleGuardar: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+interface SocialProps {
+    id: string;
+    nombre: string;
+    referencia: string;
+}
+
+interface AsideMenuProps {
+    selected: string
+    handleClickSelected: (id: string) => void
+}
+
+
+interface ModalServicioProps {
+    servicio: {
+        id: number;
+        name: string;
+        description: string;
+        img: string;
+    };
+}
+
+interface CardServicioProps {
+    servicio: {
+        id: number;
+        name: string;
+        description: string;
+        img: string;
+    };
+    index: number;
+}
+
+interface ModalProps {
+    isOpen?: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+    clases?: string;
+    modalId?: string; // ID de esta modal
+    activeId?: string | null; // ID de la modal activa global
+}
+
+interface AnimatedSelectProps {
+    name: string;
+    options: string[];
+    onChange?: (value: string) => void;
+}
