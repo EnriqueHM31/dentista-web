12a75fd449868ff99a86586c0af80bd655b80979
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-02 18:33:46 -0600

    CRear el componente aside boton

---
+++ Frontend/src/components/login/aside/BotonItemAside.tsx
@@ -0,0 +1,26 @@
+interface BotonItemAsideProps {
+    id: string;
+    label: string;
+    Icon: React.ReactNode;
+    isOpen: boolean;
+    selected: string;
+    handleClickSelected: (id: string) => void;
+}
+
+export default function BotonItemAside({ id, label, Icon, isOpen, selected, handleClickSelected }: BotonItemAsideProps) {
+    return (
+        <button
+            key={id}
+            onClick={() => handleClickSelected(id)}
+            className={`group flex cursor-pointer -center gap-3 w-full text-left p-3 rounded hover:bg-blue-900 transition ${selected === id ? "bg-blue-900" : ""
+                }`}
+            aria-label={label}
+            title={label}
+        >
+            <span className="text-2xl">
+                {Icon}
+            </span>
+            {!isOpen && <span className="truncate">{label}</span>}
+        </button>
+    )
+}
