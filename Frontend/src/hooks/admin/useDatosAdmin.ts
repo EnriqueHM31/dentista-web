5833bf199d784a69fcbc3d02be9bd69ccba4de2b
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-05 21:18:40 -0600

    Refactor useDatosAdmin a useUpdateUsuario para mejorar la gestión de actualización de usuario y agregar validación de campos

--- Frontend/src/hooks/admin/useDatosAdmin.ts
+++ Frontend/src/hooks/admin/useDatosAdmin.ts
@@ -1,15 +1,31 @@
 import { toast } from "sonner";
 
-export function useDatosAdmin() {
+export function useUpdateUsuario(cerrarMenu: () => void) {
     const handleMostrarConfirmacion = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const username = (e.currentTarget.username as HTMLInputElement).value;
+        const password = (e.currentTarget.password as HTMLInputElement).value;
+
+        if (username === "" || password === "") {
+            toast.error("Debes rellenar todos los campos");
+            return;
+        }
+
         toast("¿Estás seguro?", {
             id: "confirmacion",
             description: "Esta acción no se puede deshacer.",
             action: {
                 label: "Aceptar",
-                onClick: () => {
-                    toast.success("Acción confirmada");
+                onClick: async () => {
+                    const toastId = toast.loading("Cargando...");
+                    const { success, message } = await updateinfoUsuario(username, password);
+
+                    if (!success) {
+                        toast.error(message, { id: toastId });
+                        return;
+                    }
+                    toast.success("Acción confirmada", { id: toastId });
+                    cerrarMenu();
                 },
             },
             cancel: {
@@ -20,6 +36,21 @@ export function useDatosAdmin() {
             },
         });
     };
+
+
+    const updateinfoUsuario = async (username: string, password: string) => {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/usuario`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        const { success, message } = await response.json();
+
+        return { success, message };
+    }
 
     return {
         handleMostrarConfirmacion
