537a2f806fa18c91a64d8fbfba80dbad124a4696
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-08 19:40:00 -0600

    Agregar hook useEditarServicio para manejar la edición de servicios con confirmación de cambios

---
+++ Frontend/src/hooks/admin/useEditarServicio.ts
@@ -0,0 +1,78 @@
+import { toast } from "sonner";
+import type { ServicioResponse, Servicio } from "@/types";
+import { useState } from "react";
+
+export function useEditarServicio({ serviciosRef, formValues }: { serviciosRef: React.MutableRefObject<ServicioResponse[]>, formValues: ServicioResponse }) {
+    const [preview, setPreview] = useState<keyof Servicio | null>('name');
+
+
+    const handlePreview = (campo: keyof Servicio) => {
+        setPreview(campo);
+    };
+
+
+    const handleSubmit = async (e: React.FormEvent, id: string) => {
+        e.preventDefault();
+
+        // Verificar si hubo cambios
+
+        const cambios = (Object.keys(formValues) as (keyof Servicio)[]).filter(
+            (key) => formValues[key] !== serviciosRef.current.find((s) => s.id === id)?.[key]
+        );
+
+        if (cambios.length === 0) {
+            toast.info("No hay cambios para guardar.");
+            return;
+        }
+
+        // Si hay cambios, confirmar
+        toast("¿Estás seguro que quieres guardar los cambios?", {
+            action: {
+                label: "Guardar",
+                onClick: async () => {
+                    try {
+                        const data = cambios.reduce((acc, key) => {
+                            acc[key] = formValues[key];
+                            return acc;
+                        }, {} as Partial<ServicioResponse>);
+
+
+                        const response = await fetch(
+                            `${import.meta.env.VITE_API_URL}/servicios/${id}`,
+                            {
+                                method: "PUT",
+                                body: JSON.stringify(data),
+                                headers: {
+                                    "content-type": "application/json",
+                                }
+                            }
+                        );
+
+                        const { success, message } = await response.json();
+
+                        if (success) {
+                            toast.success("Cambios guardados correctamente");
+                        } else {
+                            toast.error(message || "Error al guardar los cambios.");
+                        }
+                    } catch (error) {
+                        toast.error("Error de red al guardar los cambios.");
+                        console.error(error);
+                    }
+                },
+            },
+            cancel: {
+                label: "Cancelar",
+                onClick: () => {
+                    toast.dismiss();
+                },
+            },
+        });
+    };
+
+    return {
+        preview,
+        handlePreview,
+        handleSubmit,
+    }
+}
