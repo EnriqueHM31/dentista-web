f2a3784a83b741e6edb9a468020ae1e4b1b8af2c
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-10 21:53:24 -0600

    Agregar ruta POST para crear nuevos servicios en ServiciosRoutes

--- backend/routes/servicios.ts
+++ backend/routes/servicios.ts
@@ -3,6 +3,8 @@ import { ServiciosController } from '../controllers/local/servicio'
 
 export const ServiciosRoutes = Router()
 
+ServiciosRoutes.post('/', ServiciosController.crearServicio)
+
 ServiciosRoutes.get('/', ServiciosController.getServicios)
 
 ServiciosRoutes.put('/:id', ServiciosController.updateServicio)
