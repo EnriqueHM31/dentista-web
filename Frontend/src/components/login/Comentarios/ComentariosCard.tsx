1d61e19022e0825c8fc4df9d564d39c2930dacfc
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-16 18:25:20 -0600

    Add ComentariosCard component to streamline comment display: Introduce a new ComentariosCard component to encapsulate the rendering of comments, improving code organization and reusability. Update Comentarios component to utilize ComentariosCard for better structure and maintainability.

---
+++ Frontend/src/components/login/Comentarios/ComentariosCard.tsx
@@ -0,0 +1,34 @@
+import Testimonio from "@/components/Inicio/Comentarios/Testimonio";
+import type { Comentario } from "@/types";
+
+interface ComentariosCardProps {
+    comentarios: Comentario[];
+    toggleCheck: (id: number) => void;
+    seleccionados: { [key: string]: boolean };
+}
+
+export default function ComentariosCard({ comentarios, toggleCheck, seleccionados }: ComentariosCardProps) {
+    return (
+        comentarios.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
+                {comentarios.map(({ id, nombre, mensaje, ranking, visible }, index) => (
+                    <Testimonio
+                        id={id}
+                        key={id}
+                        client_name={nombre}
+                        comment={mensaje}
+                        rating={ranking}
+                        index={index}
+                        visible={visible} // Este valor viene del contexto
+                        checked={!!seleccionados[id]} // Estado local controlado
+                        onCheckToggle={toggleCheck}
+                    />
+                ))}
+            </div>
+        ) : (
+            <div className="flex flex-col gap-4 items-center justify-center">
+                <h2 className="text-2xl font-bold">No hay comentarios</h2>
+            </div>
+        )
+    )
+}
--- Frontend/src/components/login/secciones/Comentarios.tsx
+++ Frontend/src/components/login/secciones/Comentarios.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { ComentariosContext } from "@/context/Comentarios";
-import Testimonio from "@/components/Inicio/Comentarios/Testimonio";
 import useVisibleComentarios from "@/hooks/admin/Comentarios/VisibleComentarios";
+import ComentariosCard from "../Comentarios/ComentariosCard";
 
 export default function Comentarios() {
     const { comentarios } = useContext(ComentariosContext);
@@ -21,29 +21,9 @@ export default function Comentarios() {
                     </button>
                 </div>
             </div>
-            {
-                comentarios.length > 0 ? (
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-4">
-                        {comentarios.map(({ id, nombre, mensaje, ranking, visible }, index) => (
-                            <Testimonio
-                                id={id}
-                                key={id}
-                                client_name={nombre}
-                                comment={mensaje}
-                                rating={ranking}
-                                index={index}
-                                visible={visible} // Este valor viene del contexto
-                                checked={!!seleccionados[id]} // Estado local controlado
-                                onCheckToggle={toggleCheck}
-                            />
-                        ))}
-                    </div>
-                ) : (
-                    <div className="flex flex-col gap-4 items-center justify-center">
-                        <h2 className="text-2xl font-bold">No hay comentarios</h2>
-                    </div>
-                )
-            }
+            <div>
+                <ComentariosCard comentarios={comentarios} toggleCheck={toggleCheck} seleccionados={seleccionados} />
+            </div>
 
 
 
