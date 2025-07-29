2abc9c461439b116dcc06a8e54bc062bf9fff3d3
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-28 19:27:31 -0600

    Refactor DatePicker component to enhance user interaction: Wrap input in a div to allow for date picker display on click, improving usability and maintaining styling consistency.

--- Frontend/src/components/general/DatePick.tsx
+++ Frontend/src/components/general/DatePick.tsx
@@ -1,16 +1,25 @@
 import type { DatePickerProps } from "@/types/Citas/types";
+import { useRef } from "react";
 
 export const DatePicker: React.FC<DatePickerProps> = ({ name, value, onChange, minDate }) => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
     return (
 
-        <input
-            id="fecha"
-            name={name}
-            type="date"
-            className="w-full px-3 py-2 cursor-pointer border border-primary rounded flex justify-between items-center focus:outline-2 focus:outline-primary mt-1"
-            value={value}
-            min={minDate}
-            onChange={(e) => onChange(e)}
-        />
+        <div
+            className="w-full"
+            onClick={() => inputRef.current?.showPicker?.()}
+        >
+            <input
+                ref={inputRef}
+                id="fecha"
+                name={name}
+                type="date"
+                className="w-full px-3 py-2 cursor-pointer border border-primary rounded appearance-none focus:outline-2 focus:outline-primary mt-1"
+                value={value}
+                min={minDate}
+                onChange={(e) => onChange(e)}
+            />
+        </div>
     );
 };
