28b0b76fea4d6b088cedd7913929d366010d1061
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-28 18:40:03 -0600

    Wrap Outlet component with CitasProvider in ServiciosPages to integrate citas context for improved state management and functionality.

--- Frontend/src/routes/ServiciosPages.tsx
+++ Frontend/src/routes/ServiciosPages.tsx
@@ -2,12 +2,15 @@
 import { Outlet } from "react-router-dom";
 import { ServicioProvider } from "@/provider/Servicios";
 import { SocialesProvider } from "@/provider/Sociales";
+import CitasProvider from "@/provider/Citas";
 
 export default function ServiciosPages() {
     return (
         <ServicioProvider>
             <SocialesProvider>
-                <Outlet />
+                <CitasProvider>
+                    <Outlet />
+                </CitasProvider>
             </SocialesProvider>
         </ServicioProvider>
     );
