d5d8cf9e220d8984d2ce6944e21f9f7cc0e799f0
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-07-30 09:22:56 -0600

    eliminado

--- backend/build/routes/login.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LoginRouter = void 0;
-const express_1 = require("express");
-const login_1 = require("../controllers/login");
-exports.LoginRouter = (0, express_1.Router)();
-exports.LoginRouter.post('/', login_1.ControllerLogin.InicioSesion);
-exports.LoginRouter.get("/autenticacion", login_1.ControllerLogin.Autenticacion);
-exports.LoginRouter.get("/logout", login_1.ControllerLogin.Logout);
--- backend/build/utils/generador.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generarIdUnico = void 0;
-const crypto_1 = require("crypto");
-const generarIdUnico = () => {
-    return (0, crypto_1.randomUUID)();
-};
-exports.generarIdUnico = generarIdUnico;
